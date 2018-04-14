0fea2da218feb7b5192155522f05d23b8ee28dfe
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-04-14 17:58:43 +0200

    Initial commit

---
+++ predavanje_windows_forms/WindowsFormsApp3/ImageViewer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ImageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.panelImage = new System.Windows.Forms.Panel();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbPateka = new System.Windows.Forms.TextBox();
+            this.lblDimenzija = new System.Windows.Forms.Label();
+            this.cbZoom = new System.Windows.Forms.CheckBox();
+            this.cbScale = new System.Windows.Forms.CheckBox();
+            this.panelImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(84, 37);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // panelImage
+            // 
+            this.panelImage.AutoScroll = true;
+            this.panelImage.Controls.Add(this.pbImage);
+            this.panelImage.Location = new System.Drawing.Point(133, 46);
+            this.panelImage.Name = "panelImage";
+            this.panelImage.Size = new System.Drawing.Size(433, 491);
+            this.panelImage.TabIndex = 1;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(4, 4);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(100, 50);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Images (*.BMP, *.JPG, *.PNG, *.GIF, *.TIFF) | *.BMP; *.JPG; *.PNG; *.GIF; .TIFF| " +
+    "All Files (*.*)| *.*";
+            // 
+            // tbPateka
+            // 
+            this.tbPateka.Location = new System.Drawing.Point(133, 13);
+            this.tbPateka.Name = "tbPateka";
+            this.tbPateka.ReadOnly = true;
+            this.tbPateka.Size = new System.Drawing.Size(433, 22);
+            this.tbPateka.TabIndex = 2;
+            // 
+            // lblDimenzija
+            // 
+            this.lblDimenzija.AutoSize = true;
+            this.lblDimenzija.Location = new System.Drawing.Point(13, 88);
+            this.lblDimenzija.Name = "lblDimenzija";
+            this.lblDimenzija.Size = new System.Drawing.Size(0, 17);
+            this.lblDimenzija.TabIndex = 3;
+            // 
+            // cbZoom
+            // 
+            this.cbZoom.AutoSize = true;
+            this.cbZoom.Location = new System.Drawing.Point(13, 88);
+            this.cbZoom.Name = "cbZoom";
+            this.cbZoom.Size = new System.Drawing.Size(102, 21);
+            this.cbZoom.TabIndex = 4;
+            this.cbZoom.Text = "Zoom small";
+            this.cbZoom.UseVisualStyleBackColor = true;
+            this.cbZoom.CheckedChanged += new System.EventHandler(this.cbZoom_CheckedChanged);
+            // 
+            // cbScale
+            // 
+            this.cbScale.AutoSize = true;
+            this.cbScale.Location = new System.Drawing.Point(13, 124);
+            this.cbScale.Name = "cbScale";
+            this.cbScale.Size = new System.Drawing.Size(88, 21);
+            this.cbScale.TabIndex = 5;
+            this.cbScale.Text = "Scale big";
+            this.cbScale.UseVisualStyleBackColor = true;
+            this.cbScale.CheckedChanged += new System.EventHandler(this.cbZoom_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 549);
+            this.Controls.Add(this.cbScale);
+            this.Controls.Add(this.cbZoom);
+            this.Controls.Add(this.lblDimenzija);
+            this.Controls.Add(this.tbPateka);
+            this.Controls.Add(this.panelImage);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Image Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.panelImage.ResumeLayout(false);
+            this.panelImage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Panel panelImage;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbPateka;
+        private System.Windows.Forms.Label lblDimenzija;
+        private System.Windows.Forms.CheckBox cbZoom;
+        private System.Windows.Forms.CheckBox cbScale;
+    }
+}
+
---
+++ predavanje_windows_forms/WindowsFormsApp3/ImageViewer/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap Image { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckState();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var res = openFileDialog1.ShowDialog();
+
+            if (res != DialogResult.Cancel)
+            {
+                Bitmap image = null;
+
+                try
+                {
+                    image = new Bitmap(openFileDialog1.FileName);
+                }
+                catch (Exception )
+                {
+                    if (image != null)
+                    {
+                        image.Dispose();
+                        image = null;
+                    }
+                }
+
+                if (image != null)
+                {
+                    panelImage.HorizontalScroll.Value = 0;
+                    panelImage.VerticalScroll.Value = 0;
+                    ShowImage(image);
+                }
+            }
+
+            CheckState();
+        }
+
+        private void ShowImage(Bitmap image)
+        {
+            SuspendLayout();
+
+            lblDimenzija.Text = image.Width + "x" + image.Height;
+            tbPateka.Text = openFileDialog1.SafeFileName;
+
+            pbImage.Size = panelImage.Size;
+            if (image.Height < panelImage.Height && image.Width < panelImage.Width)
+            {
+                pbImage.SizeMode = PictureBoxSizeMode.CenterImage;
+                if (cbZoom.Checked)
+                    pbImage.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+                if (cbScale.Checked)
+                    pbImage.SizeMode = PictureBoxSizeMode.Zoom;
+                else
+                    pbImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+
+            Bitmap oldImg = Image;
+
+            pbImage.Image = Image = image;
+
+            if (oldImg != null && oldImg != Image)
+            {
+                oldImg.Dispose();
+                oldImg = null;
+            }
+
+            pbImage.Image = image;
+            ResumeLayout();
+        }
+
+        private void cbZoom_CheckedChanged(object sender, EventArgs e)
+        {
+            panelImage.HorizontalScroll.Value = 0;
+            panelImage.VerticalScroll.Value = 0;
+            ShowImage(Image);
+        }
+
+        public void CheckState()
+        {
+            if (Image == null)
+            {
+                cbScale.Enabled = false;
+                cbZoom.Enabled = false;
+            }
+            else
+            {
+                cbScale.Enabled = true;
+                cbZoom.Enabled = true;
+            }
+        }
+        
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Size newSize = new Size(this.Width - 120, this.Height - 80);
+
+            panelImage.Size = newSize;
+            pbImage.Size = newSize;
+            panelImage.HorizontalScroll.Value = 0;
+            panelImage.VerticalScroll.Value = 0;
+            if (Image != null)
+                ShowImage(Image);
+        }
+    }
+}
