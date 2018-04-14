d9d25de892edf96147721d5b902bb3533cb8ca29
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-04-14 16:57:06 +0200

    Initial commit

---
+++ predavanje_windows_forms/WindowsFormsApp2/SystemMonitor/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SystemMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRam = new System.Windows.Forms.Label();
+            this.lblProcesor = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.pcProcesor = new System.Diagnostics.PerformanceCounter();
+            this.pcRam = new System.Diagnostics.PerformanceCounter();
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcProcesor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcRam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Слободна меморија (B)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Оптовареност на процесорот";
+            // 
+            // lblRam
+            // 
+            this.lblRam.AutoSize = true;
+            this.lblRam.Location = new System.Drawing.Point(234, 13);
+            this.lblRam.Name = "lblRam";
+            this.lblRam.Size = new System.Drawing.Size(16, 17);
+            this.lblRam.TabIndex = 2;
+            this.lblRam.Text = "?";
+            // 
+            // lblProcesor
+            // 
+            this.lblProcesor.AutoSize = true;
+            this.lblProcesor.Location = new System.Drawing.Point(234, 84);
+            this.lblProcesor.Name = "lblProcesor";
+            this.lblProcesor.Size = new System.Drawing.Size(16, 17);
+            this.lblProcesor.TabIndex = 3;
+            this.lblProcesor.Text = "?";
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(353, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(69, 30);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(353, 77);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(69, 30);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.TabStop = false;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // pcProcesor
+            // 
+            this.pcProcesor.CategoryName = "Processor";
+            this.pcProcesor.CounterName = "% Processor Time";
+            this.pcProcesor.InstanceName = "_Total";
+            // 
+            // pcRam
+            // 
+            this.pcRam.CategoryName = "Memory";
+            this.pcRam.CounterName = "Available Bytes";
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Interval = 1000;
+            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnRefresh;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(430, 128);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblProcesor);
+            this.Controls.Add(this.lblRam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "System Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcProcesor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcRam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRam;
+        private System.Windows.Forms.Label lblProcesor;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Diagnostics.PerformanceCounter pcProcesor;
+        private System.Diagnostics.PerformanceCounter pcRam;
+        private System.Windows.Forms.Timer timerRefresh;
+    }
+}
+
---
+++ predavanje_windows_forms/WindowsFormsApp2/SystemMonitor/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Environment.MachineName;
+            timerRefresh.Start();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            timerRefresh.Stop();
+            string message = "Дали сте сигурни дека сакате да ја исклучете апликацијата?";
+            string caption = "Исклучување на апликација";
+            var res = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes)
+                Close();
+            else
+                timerRefresh.Start();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lblRam.Text = pcRam.NextValue().ToString("#,000");
+            lblProcesor.Text = $"{pcProcesor.NextValue():0.0}%";
+        }
+
+        private void timerRefresh_Tick(object sender, EventArgs e)
+        {
+            btnRefresh_Click(sender, e);
+        }
+    }
+}
